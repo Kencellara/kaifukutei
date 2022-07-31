e5182d866876e75f902f8cb568f6d22881b15bfc
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-07-31 18:41:30 +0900

    修正

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		<ul class="p-footerLinkList">
 			<li><a href="<?= home_url() ?>">トップページ</a></li>
 			<li><a href="http://joe-maru.com/">丈丸渡船</a></li>
-			<li>プライバシーポリシー</li>
+			<li><a href="<?= home_url('privacy-policy') ?>">プライバシーポリシー</a></li>
 		</ul>
 		<div class="p-allRightsReserved">© 2020 kencellara.com All rights reserved.</div>
 	</footer><!-- #colophon -->
