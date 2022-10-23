191093afb74946ee2136c6da4e0f9e8f8da18860
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-10-23 22:34:11 +0900

    こだわりと歴史PC完成

--- pages/shop-info.php
+++ pages/shop-info.php
@@ -1,23 +1,27 @@
 <div class="l-contentWrapper">
-  <section class="p-accessInfoArea">
+  <section class="p-accessInfoSection">
     <h1 class="c-mainHeader">アクセス</h1>
 
-    <div class="p-addressArea">
-      <p>
-        〒516-0071<br>
-        三重県伊勢市一之木2丁目5-15
-      </p>
-    </div>
+    <div class="p-accessInfoContent">
+      <div class="p-textInfo">
+        <div class="p-addressArea">
+          <p>
+            〒516-0071<br>
+            三重県伊勢市一之木2丁目5-15
+          </p>
+        </div>
 
-    <ul class="p-accessList">
-      <li>伊勢市駅・・・徒歩7分，車3分</li>
-      <li>外宮・・・徒歩10分，車3分</li>
-      <li>内宮・・・徒歩15分，車4分</li>
-      <li>おかげ横丁・・・車10分</li>
-    </ul>
+        <ul class="p-accessList">
+          <li>伊勢市駅・・・徒歩7分，車3分</li>
+          <li>外宮・・・徒歩10分，車3分</li>
+          <li>内宮・・・徒歩15分，車4分</li>
+          <li>おかげ横丁・・・車10分</li>
+        </ul>
+      </div>
 
-    <div class="p-shopLocationMap">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.316551068653!2d136.70385701561628!3d34.49485830172156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60045af439b5957b%3A0xce96bc7ce3b38bd9!2z6KW_5rSL5paZ55CGIOmWi-emj-S6rQ!5e0!3m2!1sja!2sjp!4v1665392642145!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="p-shopLocationMap">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.316551068653!2d136.70385701561628!3d34.49485830172156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60045af439b5957b%3A0xce96bc7ce3b38bd9!2z6KW_5rSL5paZ55CGIOmWi-emj-S6rQ!5e0!3m2!1sja!2sjp!4v1665392642145!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
   </section>
 
@@ -32,21 +36,25 @@
 
   <section class="p-insideInfoSection">
     <h1 class="c-mainHeader">店内の様子</h1>
-    <figure>
-      <img src="<?= get_theme_file_uri('/assets/images/shop-info/table-seat.jpg') ?>" alt="開福亭|テーブル席" height="270px" width="404px">
-      <figcaption>テーブル席</figcaption>
-    </figure>
-    <figure>
-      <img src="<?= get_theme_file_uri('/assets/images/shop-info/counter-seat.jpg') ?>" alt="開福亭|カウンター席" height="270px" width="404px">
-      <figcaption>カウンター席</figcaption>
-    </figure>
-    <figure>
-      <img src="<?= get_theme_file_uri('/assets/images/shop-info/wc.jpg') ?>" alt="開福亭|トイレの様子" height="270px" width="404px">
-      <figcaption>トイレの様子</figcaption>
-    </figure>
-    <figure>
-      <img src="<?= get_theme_file_uri('/assets/images/shop-info/t-shirt.jpg') ?>" alt="開福亭Tシャツ" height="270px" width="404px">
-      <figcaption>開福亭Tシャツ</figcaption>
-    </figure>
+    <div class="p-insideImgRow">
+      <figure>
+        <img src="<?= get_theme_file_uri('/assets/images/shop-info/table-seat.jpg') ?>" alt="開福亭|テーブル席" height="270px" width="404px">
+        <figcaption>テーブル席</figcaption>
+      </figure>
+      <figure>
+        <img src="<?= get_theme_file_uri('/assets/images/shop-info/counter-seat.jpg') ?>" alt="開福亭|カウンター席" height="270px" width="404px">
+        <figcaption>カウンター席</figcaption>
+      </figure>
+    </div>
+    <div class="p-insideImgRow">
+      <figure>
+        <img src="<?= get_theme_file_uri('/assets/images/shop-info/wc.jpg') ?>" alt="開福亭|トイレの様子" height="270px" width="404px">
+        <figcaption>トイレの様子</figcaption>
+      </figure>
+      <figure>
+        <img src="<?= get_theme_file_uri('/assets/images/shop-info/t-shirt.jpg') ?>" alt="開福亭Tシャツ" height="270px" width="404px">
+        <figcaption>開福亭Tシャツ</figcaption>
+      </figure>
+    </div>
   </section>
 </div>
