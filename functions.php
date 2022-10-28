b6bdeed9c3a11a4f34b747fb1f94c753f8597ebf
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-10-29 00:59:17 +0900

    バージョン変更

--- functions.php
+++ functions.php
@@ -44,47 +44,47 @@ function kence_scripts(): void
 	wp_enqueue_style('fontawesome', get_theme_file_uri('/lib/font-awesome_6/css/all.min.css'), [], '6.1.1');
 
 	// style.cssはテーマ直下に置かないと効かない
-	wp_enqueue_style('kence-style', get_stylesheet_uri(), [], '1.0.0');
+	wp_enqueue_style('kence-style', get_stylesheet_uri(), [], '1.0.1');
 
 	// 共通css
-	wp_enqueue_style('common-style', get_theme_file_uri('/assets/css/common.css'), [], '1.0.0');
-	wp_enqueue_style('header-style', get_theme_file_uri('/assets/css/header.css'), [], '1.0.0');
-	wp_enqueue_style('footer-style', get_theme_file_uri('/assets/css/footer.css'), [], '1.0.0');
+	wp_enqueue_style('common-style', get_theme_file_uri('/assets/css/common.css'), [], '1.0.1');
+	wp_enqueue_style('header-style', get_theme_file_uri('/assets/css/header.css'), [], '1.0.1');
+	wp_enqueue_style('footer-style', get_theme_file_uri('/assets/css/footer.css'), [], '1.0.1');
 
 	// 共通js
-	wp_enqueue_script('hamburger-menu', get_theme_file_uri('/assets/js/hamburger-menu.js'), [], '1.0.0', /* footer= */ true);
-	wp_enqueue_script('fix-header', get_theme_file_uri('/assets/js/fix-header.js'), ['jquery'], '1.0.0');
-	wp_enqueue_script('scroll-to-top', get_theme_file_uri('/assets/js/scroll-to-top.js'), ['jquery'], '1.0.0');
+	wp_enqueue_script('hamburger-menu', get_theme_file_uri('/assets/js/hamburger-menu.js'), [], '1.0.1', /* footer= */ true);
+	wp_enqueue_script('fix-header', get_theme_file_uri('/assets/js/fix-header.js'), ['jquery'], '1.0.1');
+	wp_enqueue_script('scroll-to-top', get_theme_file_uri('/assets/js/scroll-to-top.js'), ['jquery'], '1.0.1');
 
 	if (is_front_page()) {
-		wp_enqueue_style('front-page-style', get_theme_file_uri('/assets/css/front-page.css'), [], '1.0.0');
+		wp_enqueue_style('front-page-style', get_theme_file_uri('/assets/css/front-page.css'), [], '1.0.1');
 		wp_enqueue_style('slick-style', get_theme_file_uri('/lib/slick/slick.css'), [], '1.9.0');
 		wp_enqueue_script('slick', get_theme_file_uri('/lib/slick/slick.js'), ['jquery'], '1.9.0');
-		wp_enqueue_script('my-slick', get_theme_file_uri('/assets/js/slick.js'), [], '1.0.0', /* footer= */ true);
+		wp_enqueue_script('my-slick', get_theme_file_uri('/assets/js/slick.js'), [], '1.0.1', /* footer= */ true);
 	}
 
 	if (is_single()) {
-		wp_enqueue_style('news-style', get_theme_file_uri('/assets/css/news.css'), [], '1.0.0');
+		wp_enqueue_style('news-style', get_theme_file_uri('/assets/css/news.css'), [], '1.0.1');
 	}
 
 	if (is_page('menu')) {
-		wp_enqueue_style('menu-style', get_theme_file_uri('/assets/css/menu.css'), [], '1.0.0');
+		wp_enqueue_style('menu-style', get_theme_file_uri('/assets/css/menu.css'), [], '1.0.1');
 	}
 	
 	if (is_page('shop-info')) {
-		wp_enqueue_style('shop-info-style', get_theme_file_uri('/assets/css/shop-info.css'), [], '1.0.0');
+		wp_enqueue_style('shop-info-style', get_theme_file_uri('/assets/css/shop-info.css'), [], '1.0.1');
 	}
 
 	if (is_page('history')) {
-		wp_enqueue_style('history-style', get_theme_file_uri('/assets/css/history.css'), [], '1.0.0');
+		wp_enqueue_style('history-style', get_theme_file_uri('/assets/css/history.css'), [], '1.0.1');
 	}
 
 	if (is_home()) {
-		wp_enqueue_style('archive-style', get_theme_file_uri('/assets/css/archive.css'), [], '1.0.0');
+		wp_enqueue_style('archive-style', get_theme_file_uri('/assets/css/archive.css'), [], '1.0.1');
 	}
 
 	if (is_404()) {
-		wp_enqueue_style('404-style', get_theme_file_uri('/assets/css/404.css'), [], '1.0.0');
+		wp_enqueue_style('404-style', get_theme_file_uri('/assets/css/404.css'), [], '1.0.1');
 	}
 }
 add_action( 'wp_enqueue_scripts', 'kence_scripts' );
