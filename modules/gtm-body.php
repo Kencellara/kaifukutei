26b2c440c307b29dad4b88e4485ba0bce86fce11
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-11-12 16:38:52 +0900

    GTM挿入

--- modules/gtm-body.php
+++ modules/gtm-body.php
@@ -1,1 +1,3 @@
-<!-- TODO: Google Tag Manager 挿入 -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMDL6SM" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
